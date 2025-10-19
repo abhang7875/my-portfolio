ed96c83381303084f9ba04638e4364833bea0f87
abhang7875 <nileshabhang7875@gmail.com>
2025-10-19 15:08:11 +0530

    Added Certifications Module

---
+++ src/main/java/com/portfolio/entity/Certifications.java
@@ -0,0 +1,32 @@
+package com.portfolio.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Certifications {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private Date issueDate;
+	
+	@Column
+	private String issuedBy;
+	
+	
+}
---
+++ src/main/java/com/portfolio/repository/CertificationDAO.java
@@ -0,0 +1,9 @@
+package com.portfolio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.portfolio.entity.Certifications;
+
+public interface CertificationDAO extends JpaRepository<Certifications, Integer>{
+
+}
